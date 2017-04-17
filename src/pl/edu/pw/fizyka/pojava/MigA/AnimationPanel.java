5269de0e42813fe566cfd743297888ce7fda25d6
Fisher16 <mkpubliczny@gmail.com>
2017-04-17 22:19:04 +0200

    Update AnimationPanel.java

--- src/pl/edu/pw/fizyka/pojava/MigA/AnimationPanel.java
+++ src/pl/edu/pw/fizyka/pojava/MigA/AnimationPanel.java
@@ -1,18 +1,16 @@
 package pl.edu.pw.fizyka.pojava.MigA;
 
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.geom.Line2D;
+//import java.awt.event.ActionListener;
+//import java.awt.geom.Line2D;
 import pl.edu.pw.fizyka.pojava.MigA.Droplet;
 import javax.swing.*;
 
 public class AnimationPanel extends JPanel  {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 3960445104787308557L;
 	int width=this.getWidth();
 	int height=this.getHeight();
+	int gap=0;
 	//int dropx=width/2;
 	//int dropy=height/2;
 	
@@ -23,7 +21,7 @@ public class AnimationPanel extends JPanel  {
 	public AnimationPanel() {
 		this.setBackground(Color.darkGray);
 	}
-
+	
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -34,18 +32,29 @@ public class AnimationPanel extends JPanel  {
         graph.setColor(Color.blue);
         
         graph.fillOval(Droplet.x, Droplet.y, Droplet.diam, Droplet.diam+10);     
-        // capacitor
+        /*// capacitor
 	
 	    Line2D lin = new Line2D.Float(180, 400, 300, 400);
 	    Line2D lin2 = new Line2D.Float(180, 400-Droplet.dist, 300, 400-Droplet.dist);
 	    graph.draw(lin);
-	    graph.draw(lin2);
-	    
-	   
+	    graph.draw(lin2);*/
+        
+	    //cap
+	    g.setColor(Color.BLACK);
+		int w=200;
+        g.fillRect((this.getWidth()-w)/2,this.getHeight()-50, w,20);
+        g.drawRect((this.getWidth()-w)/2,this.getHeight()-gap-70, w,20);
 	   
 
 	}
-	 ActionListener taskPerformer = new ActionListener() {
+	
+	
+	public void setGap(int Gap){
+		gap=Gap;
+		this.repaint();
+	}
+	
+	/* ActionListener taskPerformer = new ActionListener() {
 	      public void actionPerformed(ActionEvent evt) {
 	    	//fall stop in capacitor 
 	        	if(y>=halfcap && Droplet.volts == 5081){
@@ -70,9 +79,7 @@ public class AnimationPanel extends JPanel  {
 	}*/
 	
 	/*public void iterate(double dt){
-
 	    List<Ball> balls = ballContainer.getBalls();
-
 	    for (int ii=0;ii<balls.size(); ii++){
 	        Ball b1 = balls.get(ii);
 	        checkCollisions(ballContainer, b1, ii);
