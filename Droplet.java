8be753ca01e1a16f75781b82d2fdc9775ae831e2
egeath <romanowskagata@o2.pl>
2017-04-17 17:48:07 +0200

    Add files via upload

---
+++ Droplet.java
@@ -0,0 +1,20 @@
+package pl.edu.pw.fizyka.pojava.MigA;
+
+public class Droplet {
+	//position
+	int x,y;
+	
+	
+	public Droplet() {
+		//start position
+		x=230;
+		y=80;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Droplet drop =new Droplet();
+	}
+
+}
